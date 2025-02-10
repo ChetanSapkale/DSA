3eb0c2930e6042039d43b0aa4e49eb58c4a46a82
Chetan Sapkale <148869243+ChetanSapkale@users.noreply.github.com>
2025-02-10 16:18:37 +0530

    Add files via upload

---
+++ Loop_Basics/1toN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cout << "Enter Number:-";
+	cin >> n;
+	
+    int i=1;
+    while(i<=n)
+    {
+    	cout << i << endl;
+    	i++;
+	}
+  
+  
+  
+  return 0;
+}
